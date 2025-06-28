37d1834f00ca9c577f6f5f93f119517503ecfcc8
soura1334 <151785439+soura1334@users.noreply.github.com>
2025-06-28 21:39:34 +0530

    Implemented design of Login page, Navbar and Footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return <div className="text-center bg-zinc-800 p-5">
+        <p>Traitly © 2025. All rights reserved.</p>
+    </div>
+}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,21 +4,27 @@ export default function Navbar() {
   return (
     <nav className="bg-zinc-800 flex p-4 justify-around items-center ">
       <div className="flex gap-4 relative">
-          <img src="/logo.png" className="h-10" />
-          <NavLink to="/" className='text-4xl'>Traitly</NavLink>
+        <img src="/logo.png" className="h-10" />
+        <NavLink to="/" className="text-4xl">
+          Traitly
+        </NavLink>
       </div>
-      <ul className="flex items-center justify-center gap-8 text-purple-600 relative">
+      <ul className="flex items-center justify-center gap-8 relative">
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/">
+            {({ isActive }) => (
+              <span className={isActive ? "text-purple-600" : ""}>Home</span>
+            )}
+          </NavLink>
         </li>
-        <li>
-            Contact
-        </li>
-        <li>
-            About
-        </li>
-        <NavLink to='/login'>
-            <button className="bg-purple-600 hover:bg-purple-800 text-white p-2 rounded-lg cursor-pointer">Login</button>
+        <li>Contact</li>
+        <li>About</li>
+        <NavLink to="/login">
+          {({ isActive }) => (
+            <button className={`${isActive ? "bg-purple-600 hover:bg-purple-800" :"bg-blue-500 hover:bg-blue-800"} text-white p-2 rounded-lg cursor-pointer`}>
+              Login
+            </button>
+          )}
         </NavLink>
       </ul>
     </nav>
