a70b251329792c7df664b172d21f9ea91cb41901
soura1334 <151785439+soura1334@users.noreply.github.com>
2025-06-29 00:06:55 +0530

    Improved Home and Login pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer"
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <div className="flex justify-center items-center min-h-[85vh]">
+        <div className="min-h-[70vh] min-w-[80vw] text-center">
+          <p className="text-5xl font-semibold">Discover What Tweets Say About You</p>
+          <p className="my-10 text-xl text-gray-500">Traitly analyzes your X profile to reveal your personality traits and emotional tone using AI</p>
+          <NavLink to='/login'>
+          <button className="bg-blue-500 p-5 rounded-xl hover:bg-blue-900 cursor-pointer relative">Analyze My Profile</button>
+          </NavLink>
+        </div>
+      </div>
       <Footer />
     </div>
   );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,23 +5,23 @@ export default function Login() {
   return (
     <div>
       <Navbar />
-      <div className="flex justify-center items-center min-h-screen">
+      <div className="flex justify-center items-center min-h-[85vh]">
         <form className="min-h-[70vh] min-w-[30vw] max-w-[30vw] bg-zinc-800 rounded-md px-10 flex flex-col justify-evenly" onSubmit={(e)=> e.preventDefault()}>
           <p className="text-center text-3xl">Login</p>
           <div className="relative">
             <label className="text-lg block mb-2">Username</label>
-            <input type="text" className="bg-gray-500 w-full rounded-sm" />
+            <input type="text" className="bg-gray-500 w-full rounded-sm h-10" />
           </div>
           <div className="relative">
             <label className="text-lg block mb-2">Password</label>
-            <input type="password" className="bg-gray-500 w-full rounded-sm" ></input>
-          </div>
-          <div className="flex justify-center ">
-            <button className="bg-blue-500 p-3 px-5 rounded-xl relative cursor-pointer hover:bg-blue-900">Login</button>
+            <input type="password" className="bg-gray-500 w-full rounded-sm h-10" ></input>
           </div>
           <div className="flex justify-between">
             <button>Forgot Password?</button>
             <button>Sign Up</button>
+          </div>
+          <div className="flex justify-center ">
+            <button className="bg-blue-500 p-3 px-5 rounded-xl relative cursor-pointer hover:bg-blue-900">Login</button>
           </div>
         </form>
       </div>
